12bf366b4abb015baa9044f783d47ae38a855e79
Andrey Chernyshev <andrey.chernyshev@simbirsoft.com>
2021-10-28 22:06:57 +0300

    build 1

---
+++ Phlebotomy/Classes/Misc/Appearance.swift
@@ -0,0 +1,21 @@
+//
+//  Appearance.swift
+//  Ged
+//
+//  Created by Andrey Chernyshev on 23.04.2021.
+//
+
+import UIKit
+
+struct Appearance {
+    static let backgroundColor = UIColor(integralRed: 242, green: 245, blue: 255)
+    
+    static let mainColor = UIColor(integralRed: 62, green: 139, blue: 255)
+    static let mainColorAlpha = UIColor(integralRed: 62, green: 139, blue: 255, alpha: 0.3)
+    
+    static let takeFreeTestColor = UIColor(integralRed: 36, green: 234, blue: 178)
+    
+    static let progress1Color = UIColor(integralRed: 62, green: 139, blue: 255)
+    static let progress2Color = UIColor(integralRed: 36, green: 234, blue: 178)
+    static let progress3Color = UIColor(integralRed: 251, green: 49, blue: 104)
+}
