29f8b521e48a2bcaf88ec032309f4472f092776c
administrator <administrator@administrators-Mac-mini.local>
2021-10-28 17:01:58 -0400

    group

--- Phlebotomy/Classes/Service/Stats/Share/StatsShareManager.swift
+++ Phlebotomy/Classes/Service/Stats/Share/StatsShareManager.swift
@@ -16,7 +16,7 @@ final class StatsShareManager {
 // MARK: Public
 extension StatsShareManager {
     func sharedContainerURL() -> URL? {
-        FileManager.default.containerURL(forSecurityApplicationGroupIdentifier: "group.phlebotomy.widgets")
+        FileManager.default.containerURL(forSecurityApplicationGroupIdentifier: "group.phlebotomy.stats")
     }
     
     @discardableResult
