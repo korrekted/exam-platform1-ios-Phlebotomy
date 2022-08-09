bcb6275ea9d28adc7174209257b2e6ce3ad405ff
akonst17 <akonst17@gmail.com>
2022-08-09 13:25:16 +0300

    обновления Nursing; build 6

--- Phlebotomy/Classes/Misc/Appearance.swift
+++ Phlebotomy/Classes/Misc/Appearance.swift
@@ -8,18 +8,14 @@
 import UIKit
 
 struct Appearance {
-    static let backgroundColor = UIColor(integralRed: 242, green: 245, blue: 255)
-    
     static let mainColor = UIColor(integralRed: 62, green: 139, blue: 255)
     static let mainColorAlpha = UIColor(integralRed: 62, green: 139, blue: 255, alpha: 0.3)
-    
-    static let takeFreeTestColor = UIColor(integralRed: 36, green: 234, blue: 178)
-    
-    static let progress1Color = UIColor(integralRed: 62, green: 139, blue: 255)
-    static let progress2Color = UIColor(integralRed: 36, green: 234, blue: 178)
-    static let progress3Color = UIColor(integralRed: 251, green: 49, blue: 104)
-    
+    static let secondColor = UIColor(integralRed: 251, green: 49, blue: 104)
+    static let thirdColor = UIColor(integralRed: 36, green: 234, blue: 178)
     static let successColor = UIColor(integralRed: 46, green: 190, blue: 161)
     static let errorColor = UIColor(integralRed: 255, green: 115, blue: 99)
     static let warningColor = UIColor(integralRed: 255, green: 155, blue: 38)
+    static let backgroundColor = UIColor(integralRed: 242, green: 245, blue: 255)
+    static let greyColor = UIColor(integralRed: 75, green: 81, blue: 102)
+    static let blackColor = UIColor(integralRed: 19, green: 24, blue: 42)
 }
--- Phlebotomy/Classes/Misc/GlobalDefinitions.swift
+++ Phlebotomy/Classes/Misc/GlobalDefinitions.swift
@@ -17,6 +17,7 @@ final class GlobalDefinitions {
     static let amplitudeApiKey = "b503251969f4b1d7901d2f7d1388d476"
     static let appsFlyerApiKey = "DCciCfYXjMQ8QnkdCg8qzk"
 
+    static let appStoreUrl = "https://apps.apple.com/app/nha/id1591622159"
     static let appleAppID = "1591622159"
 
     static let termsOfServiceUrl = "https://quantresear.ch/terms.html"
