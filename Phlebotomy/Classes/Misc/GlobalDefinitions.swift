2bcd8a46e9d8a1c0c67ed23e349701202d058b85
akonst17 <akonst17@gmail.com>
2022-09-21 15:29:04 +0300

    интеграция sdk 2.0; новые урлы; build 9

--- Phlebotomy/Classes/Misc/GlobalDefinitions.swift
+++ Phlebotomy/Classes/Misc/GlobalDefinitions.swift
@@ -6,21 +6,24 @@
 //
 
 final class GlobalDefinitions {
-    static let domainUrl = "https://exams.korrekted.com"
+    static let domainUrl = "https://exams3.mastrapi.com"
     static let apiKey = "AlWAqBwqtYFkifcM"
-
-    static let sdkDomainUrl = "https://analytics.korrekted.com"
-    static let sdkApiKey = "z6GVbRzpu265JrKk"
 
     static let applicationTag = "EXAM_PHLEBOTOMY"
 
     static let amplitudeApiKey = "b503251969f4b1d7901d2f7d1388d476"
     static let appsFlyerApiKey = "DCciCfYXjMQ8QnkdCg8qzk"
+    
+    static let sdkDomainUrl = "https://analytics.mastrapi.com"
+    static let sdkApiKey = "z6GVbRzpu265JrKk"
+    
+    static let otterScaleHost = "https://analytics2.mastrapi.com"
+    static let otterScaleApiKey = "HyiOm765Fu3yWRKy"
 
     static let appStoreUrl = "https://apps.apple.com/app/nha/id1591622159"
     static let appleAppID = "1591622159"
 
-    static let termsOfServiceUrl = "https://quantresear.ch/terms.html"
-    static let privacyPolicyUrl = "https://quantresear.ch/policy.html"
-    static let contactUsUrl = "https://quantresear.ch/contact.html"
+    static let termsOfServiceUrl = "https://mastrapi.com/terms"
+    static let privacyPolicyUrl = "https://mastrapi.com/policy"
+    static let contactUsUrl = "https://mastrapi.com/contact"
 }
